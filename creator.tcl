d82b7a045c5c8fef2d4f1f003f1c4d7ac23d1818
marcm24 <marcmelendez24@gmail.com>
2024-08-09 22:15:48 -0400

    added d flip flop proc

--- creator.tcl
+++ creator.tcl
@@ -5,15 +5,35 @@
 # [lindex $argv 0] 0th index of argv
 
 # proc for all components
-proc dflipflop {} {
-    puts "creating d flip flop"
-    set fp [open "dff.sv" w+]
-}
 
 proc 2mux1 {} {
     puts "creating 2:1 mux"
-    close [open "2mux1.tv" w+]
+    close [open "2mux1.tv" a+]
 }
+
+proc dflipflop {bits} {
+    puts "creating d flip flop"
+    
+    set bitio [expr $bits - 1]
+
+    set dff {
+    module dff(input logic clk,
+               input logic reset,
+               input logic \[$bitio:0]\ d, # need to subtract 1 from bits
+               output logic \[$bitio:0]\ q); 
+    
+    always_ff @(posedge clk)
+        if (reset) q <= $bits'b0;
+        else       q <= d;
+    
+    endmodule
+    }
+    
+    set fp [open "dff.sv" a+]
+    puts $fp [subst $dff]
+
+}
+
 
 # ----------------------------------------
 
@@ -23,5 +43,5 @@ if {[lindex $argv 0] == "2:1mux"} {
 }
 
 if {[lindex $argv 0] == "dff"} {
-    dflipflop
+    dflipflop [lindex $argv 1]
 }
